c865af451a75b97c78889f91f57e7f2b42b14064
Karel Lenc <karel@karel-mbp.corp.dropbox.com>
2015-08-20 23:51:11 -0700

    Added the tessocr module.

---
+++ src/imgproc/tessocr.cpp
@@ -0,0 +1,56 @@
+#include "tessocr.h"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <assert.h>
+
+#include <sstream>
+
+using namespace std;
+using namespace cv;
+
+TessOCR::TessOCR(double text_thr, const char* whitelist_char,
+                 tesseract::PageSegMode psm)
+{
+    this->api = new tesseract::TessBaseAPI();
+    if (api->Init(NULL, "eng")) {
+        fprintf(stderr, "Could not initialize tesseract.\n");
+    }
+    api->SetPageSegMode(psm);
+    api->SetVariable("tessedit_char_whitelist", whitelist_char);
+    this->text_thr = text_thr;
+}
+
+void TessOCR::analyze(Mat& frame, std::vector<OCRCase> &ocr_cases, bool draw_binarization) {
+    vector<OCRCase>::iterator it = ocr_cases.begin();
+    for (; it != ocr_cases.end(); ++it) {
+        Mat sel, gray;
+        sel = frame(it->roi);
+        if (sel.channels() == 3)
+            cvtColor(sel, gray, CV_BGR2GRAY);
+        else
+            sel.copyTo(gray);
+
+        // Make sure that row step is equal number of rows
+        assert(gray.isContinuous());
+        // Threshold - keep only the whites...
+        threshold(gray, gray, this->text_thr, 255, THRESH_BINARY_INV);
+        if (draw_binarization) {
+            sel.setTo(Scalar(0,0,0), gray < 100);
+            sel.setTo(Scalar(0,255,0), gray > 100);
+        }
+
+        this->api->SetImage((uchar*)gray.data, gray.cols, gray.rows, 1, gray.step);
+        char* outText = this->api->GetUTF8Text();
+        std::string str(outText);
+        // trim the text
+        str.erase(str.find_last_not_of(" \n\r\t")+1);
+        it->update(str, api->MeanTextConf());
+
+        delete outText;
+    }
+}
+
+
+TessOCR::~TessOCR()
+{
+    delete this->api;
+}
---
+++ src/imgproc/tessocr.h
@@ -0,0 +1,62 @@
+#ifndef GAMEOCR_H
+#define GAMEOCR_H
+
+#include <vector>
+#include <tesseract/baseapi.h>
+#include <opencv2/core/core.hpp>
+
+/**
+ * @brief Structure holding definition and results of an OCR case
+ */
+struct OCRCase{
+    cv::Rect roi; /** ROI in the input frame (absolute coordinates) */
+    std::string text; /** The detected text */
+    int confidence; /** Confidence of the text (int, 0-100, 100 tha best) */
+    const int min_confidence; /** Minimal confidence to update the text */
+
+    OCRCase(cv::Rect roi, std::string text, int min_confidence) :
+        roi(roi), text(text), confidence(100), min_confidence(min_confidence)
+        {}
+
+    /**
+     * @brief Update the text results only when confidence higher than min_confidence
+     */
+    void update(std::string text, int confidence) {
+        if (confidence > this->min_confidence) {
+            this->confidence = confidence;
+            this->text = text;
+        }
+    }
+};
+
+/**
+ * @brief Thinniest wrapper around tesseract OCR
+ */
+class TessOCR
+{
+public:
+    /**
+     * @brief Construct a game screen text recogniser. Assumes the text is brighter in background
+     * in the binarization operation (simple threshold over some value).
+     * @param text_thr Minimal text brightnes in GrayScale for the foreground segmentation
+     * @param whitelist_char String of allowed characters
+     * @param psm Tesseract mode, default PSM_SINGLE_WORD
+     */
+    TessOCR(double text_thr=200, const char* whitelist_char="0123456789:%",
+            tesseract::PageSegMode psm=tesseract::PSM_SINGLE_WORD);
+    ~TessOCR();
+
+    /**
+     * @brief Perform the OCR, calls update on each OCR case.
+     * @param frame Input image frame
+     * @param ocr_cases List of OCR cases
+     * @param draw_binarization Do draw binarization results to the input frame, false by default
+     */
+    void analyze(cv::Mat& frame, std::vector<OCRCase>& ocr_cases, bool draw_binarization=false);
+protected:
+    bool is_initialized;
+    tesseract::TessBaseAPI *api;
+    double text_thr;
+};
+
+#endif // GAMEOCR_H
